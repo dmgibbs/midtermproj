2ef01ef733b19c2a634b4fa7ba0f1e85e5a82336
lexy5251 <xixi5251@gmail.com>
2018-08-26 04:51:53 +0000

    added api engine routine to get the proper category

--- server.js
+++ server.js
@@ -3,18 +3,101 @@
 require('dotenv').config();
 const model = require('./model.js');
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
+const bodyParser = require("body-parser");
+const sass = require("node-sass-middleware");
+const app = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig = require("./knexfile");
+const knex = require("knex")(knexConfig[ENV]);
+const morgan = require('morgan');
+const knexLogger = require('knex-logger');
 const cookieSession = require('cookie-session');
+
+
+
+//const http = require('http');
+//const request = require('request');
+var pos = require('pos');
+const pd = require('paralleldots');
+
+// Be sure to set your API key
+pd.apiKey = "WsJx3P7cFx5CLT2eLaVrfrh6RhaNFSEYKoKgKrDZcSA";
+
+const category = {
+  "movie": [
+    "film", "entertainment",
+    "play", "concert",
+    "cinema"
+  ],
+  "restaurant": [
+    "food", "dinner", "lunch", "breakfast",
+    "supper", "meal", "eat"
+  ],
+  "products": [
+    "item", "store", "product", "goods",
+    "general", "something"
+  ],
+  "Books": ["read", "play", "paper", "look", "see", "magazine", "novel"]
+}
+
+
+const stripInput = function() {
+  var entry = process.argv.slice(2).join(" ");
+  var words = new pos.Lexer().lex(entry);
+  var tagger = new pos.Tagger();
+  var taggedWords = tagger.tag(words);
+  var verbs = [];
+  let pushedItems = 0;
+  for (i in taggedWords) {
+    var taggedWord = taggedWords[i];
+    var word = taggedWord[0];
+    var tag = taggedWord[1];
+    if (tag.includes("VB") || tag.includes("NN")) {
+      if (pushedItems < 8) {
+        verbs.push(word);
+        pushedItems++;
+      }
+    }
+    console.log(word + " /" + tag);
+  }
+
+  return verbs.join(" ");
+}
+
+const res = stripInput();
+
+
+
+
+// evaluateScores()
+//   .then((result) => {
+//     data = JSON.parse(result);
+//     answer = data['taxonomy'].sort();
+//     var value = 0;
+//     switch (answer[0].tag) {
+//       case "movie":
+//         value = 1;
+//         break;
+//       case "books":
+//         value = 2;
+//         break;
+//       case "restaurant":
+//         value = 3;
+//         break;
+//       case "products":
+//         value = 4;
+//     }
+//     console.log(" Your category selected is: ", answer[0].tag);
+//     console.log("which as a category id of :", value)
+//   }).catch((error) => {
+//     console.log(error);
+//   })
+
+
+
 
 // Seperated Routes for each Resource
 //const usersRoutes = require("./routes/users");
@@ -29,11 +112,11 @@ app.use(morgan('dev'));
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 app.use(cookieSession({
-    name: 'session',
-    signed: false,
+  name: 'session',
+  signed: false,
 
-    // Cookie Options
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
 app.set("view engine", "ejs");
@@ -54,26 +137,26 @@ app.use(express.static("public"));
 
 // Home page - GET
 app.get("/", (req, res) => {
-let templateVars = {
-  id : req.session.id
-};
-  res.render("index",templateVars);
+  let templateVars = {
+    id: req.session.id
+  };
+  res.render("index", templateVars);
 });
 
 // Register page - POST
 app.get("/register", (req, res) => {
-    res.render("register");
+  res.render("register");
 });
 
 
 //Login page - GET
 app.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
 
 app.get("/profile", (req, res) => {
-    res.render("profile");
+  res.render("profile");
 });
 
 
@@ -87,45 +170,101 @@ app.post("/register", (req, res) => {
 
 
 //POST - /todo/add - todolist page
-app.post("/todo/add", (req,res) => {
-  console.log("req.session.id00000",req.session.id);
-  knex('todolist')
-    .insert({description: req.body.description, duration: req.body.duration, status: req.body.todo, user_id: req.session.id, cat_id: 3})
-    .then((results) => {
-    console.log("req.body",req.body);
-     res.redirect("/");
-    });
+app.post("/todo/add", (req, res) => {
+  function evaluateScores(description) {
+    pd.customClassifier(description, category)
+      .then((response) => {
+        let evaluationData = JSON.parse(response)
+        let answer = evaluationData['taxonomy'].sort();
+        console.log("I am inserting this tag",answer[0].tag);
+        var value = 0;
+        switch (answer[0].tag) {
+          case "movie":
+            value = 1;
+            break;
+          case "books":
+            value = 2;
+            break;
+          case "restaurant":
+            value = 3;
+            break;
+          case "products":
+            value = 4;
+        }
+        console.log("value of string:", value);
+        // knexfunction();
+        knex('todolist')
+          .insert({ description: req.body.description, duration: req.body.duration, status: req.body.todo, user_id: req.session.id, cat_id: value })
+          .then((results) => {
+            console.log("req.body", req.body);
+            res.redirect("/");
+
+          });
+      })
+  }
+
+  console.log(req.body);
+
+  function giveDescription() {
+    return new Promise((resolve, reject) => {
+      resolve(req.body.description);
+    })
+  };
+
+  giveDescription()
+    .then((result) => {
+      evaluateScores(result)
+    }).catch((err) => {
+      console.log(err);
+    })
+
+
+  // evaluateScores()
+  //   .then((result) => {
+  //     data = JSON.parse(result);
+  //     console.log("got to evaluate");
+
+
+  //   console.log(" Your category selected is: ", answer[0].tag);
+  //   console.log("which as a category id of :", value)
+
+  //  //---------------------------------------------------------------------
+
+  // }).catch((error) => {
+  //   console.log(error);
+  // });
+
 });
 
 
 //POST - /login
 app.post("/login", (req, res) => {
   console.log("req.body is: ", req.body);
-   knex
-    .select('id','email','password')
+  knex
+    .select('id', 'email', 'password')
     .from('users')
-    .where('email',req.body.email )
-    .andWhere( 'password', req.body.password)
-    .then((results) =>{
+    .where('email', req.body.email)
+    .andWhere('password', req.body.password)
+    .then((results) => {
       console.log(results);
-     req.session.id = results[0].id;
-     console.log("req.session.id ~~~~~~~~",req.session.id);
+      req.session.id = results[0].id;
+      console.log("req.session.id ~~~~~~~~", req.session.id);
 
       res.redirect("/");
-      })
+    })
 });
 
 
 //GET - get information from todolist
 app.get("/todo/list", (req, res) => {
-  console.log("get to do list: ",req.session.id);
-   knex('todolist')
-  .select('description','duration','status','user_id','cat_id')
-  .where({'user_id': req.session.id})
-  .asCallback(function(err, rows) {
-  res.send(rows);
+  console.log("get to do list: ", req.session.id);
+  knex('todolist')
+    .select('description', 'duration', 'status', 'user_id', 'cat_id')
+    .where({ 'user_id': req.session.id })
+    .asCallback(function(err, rows) {
+      res.send(rows);
 
-  });
+    });
 
 });
 
@@ -144,7 +283,7 @@ app.post("/users/:id/edit", (req, res) => {
 });
 
 //edit form
-app.get("/users/:id/edit", (req,res) => {
+app.get("/users/:id/edit", (req, res) => {
 
 });
 
